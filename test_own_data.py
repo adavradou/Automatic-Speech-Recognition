655175122ee0af0caa7804928dc11c0100a2192a
adav <30274421+adavradou@users.noreply.github.com>
2019-11-25 21:54:13 +0200

    Add files via upload
    This script is for testing on own data and contains the followings:
    1) Merging multiple .wav files, for creating a sequence with digits from e.g. youtube.
    2) Recording and creating a digit sequence with your own voice.
    3) Reading the sequence, splitting it into the corresponding digits and predicting.

--- test_own_data.py
+++ test_own_data.py
@@ -1,6 +1,9 @@
 import os
+import numpy as np
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
+import sounddevice as sd
+import soundfile as sf
 import librosa   # for audio processing
 import IPython.display as ipd
 from keras.utils import np_utils
@@ -13,6 +16,7 @@ from keras.models import load_model
 current_directory = os.getcwd()
 filepath = current_directory + '/model_conv_kostas.hdf5'
 model = load_model(filepath)
+classes = np.load('classes_conv.npy')
 
 
 def predict(audio):
@@ -21,41 +25,57 @@ def predict(audio):
     return classes[index]
 
 ######## MERGE MULTIPLE AUDIO FILES IN ONE ############
-filepath = os.getcwd() + "/testing_numbers/"
-
-#Choose the numbers with the prefered order to test. 
-silence = AudioSegment.from_wav(filepath+"silence.wav")
-sound1 = AudioSegment.from_wav(filepath+"2.wav")
-sound2 = AudioSegment.from_wav(filepath+"4.wav")
-sound3 = AudioSegment.from_wav(filepath+"6.wav")
-sound4 = AudioSegment.from_wav(filepath+"8.wav")
-sound5 = AudioSegment.from_wav(filepath+"7.wav")
-
-#Combine them in one audio file.
-combined_sounds = sound1 + silence + sound2 + silence + sound3 + silence + sound4 + silence + sound5
-combined_sounds.export("testing_sequence.wav", format="wav")
-
-
-######### SPLIT THE PHRASE INTO WORDS ############
-#sound_file = AudioSegment.from_wav("testing_sequence.wav")
-#audio_chunks = split_on_silence(sound_file, 
-#    # must be silent for at least half a second
-#    min_silence_len=1000,
+#filepath = os.getcwd() + "/testing_numbers/"
 #
-#    # consider it silent if quieter than -36 dBFS
-#    silence_thresh=-36
-#)
+##Choose the numbers with the prefered order to test. 
+#silence = AudioSegment.from_wav(filepath+"silence.wav")
+#sound1 = AudioSegment.from_wav(filepath+"2.wav")
+#sound2 = AudioSegment.from_wav(filepath+"4.wav")
+#sound3 = AudioSegment.from_wav(filepath+"6.wav")
+#sound4 = AudioSegment.from_wav(filepath+"8.wav")
+#sound5 = AudioSegment.from_wav(filepath+"7.wav")
 #
-##Export each digit of a sequence in a .wav file.
-#for i, chunk in enumerate(audio_chunks):
-#    out_file = "chunk{0}.wav".format(i)
-#    print("exporting", out_file)
-#    chunk.export(out_file, format="wav")
-#    
-#    samples, sample_rate = librosa.load("chunk{0}.wav".format(i), sr = 16000)
-#    samples = librosa.resample(samples, sample_rate, 8000)
-#    ipd.Audio(samples,rate=8000)  
-#    
-#    #predict(samples)
-#    print("Text:", predict(samples))
+##Combine them in one audio file.
+#combined_sounds = sound1 + silence + sound2 + silence + sound3 + silence + sound4 + silence + sound5
+#combined_sounds.export("testing_sequence.wav", format="wav")
+
+
+#CREATE YOUR OWN AUDIO FILE TO TEST ON.
+#samplerate = 16000  
+#duration = 11 #seconds
+#filename = 'testing_sequence.wav'
+#print("start")
+#mydata = sd.rec(int(samplerate * duration), samplerate=samplerate,
+#    channels=1, blocking=True)
+#print("end")
+#sd.wait()
+#sf.write(filename, mydata, samplerate)
+
+########## SPLIT THE PHRASE INTO WORDS ############
+sound_file = AudioSegment.from_wav("testing_sequence_agapi.wav")
+audio_chunks = split_on_silence(sound_file, 
+    # must be silent for at least half a second
+    min_silence_len=500,
+
+    # consider it silent if quieter than -64 dBFS
+    silence_thresh=-56
+)
+
+#Export each digit of a sequence in a .wav file.
+for i, chunk in enumerate(audio_chunks):
+    out_file = "chunk{0}.wav".format(i)
+    print("exporting", out_file)
+    chunk.export(out_file, format="wav")
+    
+    #samples, sample_rate = librosa.load("chunk{0}.wav".format(i), sr = 16000)
+    samples, sample_rate = librosa.load("chunk{0}.wav".format(i), sr=8000,mono=True)
+    samples = librosa.resample(samples, sample_rate, 8000)
+    ipd.Audio(samples,rate=8000)  
+    
+    #If the sample size is less than 8000, add zeros.
+    featuresSize=sample_rate
+    samples = np.pad(samples, (0, featuresSize - len(samples)), 'constant')
+
+    #predict(samples)
+    print("Text:", predict(samples))
 
