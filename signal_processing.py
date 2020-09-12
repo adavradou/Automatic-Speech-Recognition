09454b673e9822818ccf7aed0a479b7e05435376
Agapi Davradou <adavradou@gmail.com>
2020-09-12 23:13:25 +0300

    deleted unnecessary files

--- signal_processing.py
+++ signal_processing.py
@@ -24,42 +24,10 @@ import random
 
 from scipy import signal
 
-
-def fourierTransform(fs,y,label,no_of_peaks):
-    # Number of samplepoints
-    N = len(y)
-    # sampling period
-    T = 1.0 / fs
-
-    yf = fft(y)
-
-    xf = np.linspace(0.0, 1.0/(2.0*T), N/2) * 0.5 # No need for both semi-axis the rfequency domain in Hertz
-    yf = 2.0 * np.abs(yf[:N//2]) # The frequency magnitude
-
-    #plt.plot(xf,yf,label=str(label) + ' - file :' + file.name +"N" + str(N)) # plot the fourier transform
-    #plt.show()
-
-    # Find dominant freq peak centers and train matching them with their frequency domains
-    peaks=[]
-
-    i=0
-    step=10 # Peaks greter than step Hz in distance
-    for point in yf:
-        peaks.append([xf[i],point])
-        i+=1
-
-    # Choose greatest magnitude
-    peaks=sorted(peaks, key=lambda x: x[1],reverse=True)
-    peaks=peaks[:no_of_peaks]
-
-    return xf,yf,N,peaks
-
-
-
 # The voiced speech of a typical adult male will have a fundamental frequency from 85 to 180 Hz, and that of a typical adult female from 165 to 255 Hz.
 #def bandpassIIRFilter(data, fs, lowcut=300, highcut=3400, order=4):#telephony - i xroia poy prokyptei apo tis anwteres armonikes einai mallon axristi
 #def bandpassIIRFilter(data, fs, lowcut=60, highcut=800, order=4):
-def bandpassIIRFilter(data, fs, lowcut=85, highcut=255, order=4):
+def bandpassIIRFilter(data, fs, lowcut=60, highcut=2400, order=4):
     nyq = 0.5 * fs
     low = lowcut / nyq
     high = highcut / nyq
@@ -69,95 +37,79 @@ def bandpassIIRFilter(data, fs, lowcut=85, highcut=255, order=4):
 
     return y
 
+def process_dataset(labels,train_audio_path):
+    i = 1
+    durations = []
+    sum = 0
+    processedAudioFiles = []
+    all_label = []
+    sr = 8000
+    print("Signal processing")
+    for label in labels:
+        files = list(train_audio_path.glob(label + '/*.wav'))
+        print("processing " + label)
+        for file in files:
+            originalSignal, sr = librosa.load(str(file), sr=8000, mono=True)
+            processedSignal = bandpassIIRFilter(originalSignal, sr)
+            processedSignal, index = librosa.effects.trim(processedSignal)
+            duration = librosa.get_duration(processedSignal)
+            sum+=duration
+            processedAudioFiles.append(processedSignal)
+            i+=1
+            all_label.append(label)
+            if sr != 8000:
+                print(sr)
 
-current_directory = os.getcwd()
+    meanDuration=sum/i
+    return processedAudioFiles,all_label,sr,meanDuration
 
-# Choose audio dataset
-train_audio_path = Path('./free-spoken-digit-dataset-medium') #Audio Path
+def process_predict_dataset(labels,train_audio_path):
+    i = 1
+    durations = []
+    sum = 0
+    processedAudioFiles = []
+    all_label = []
+    sr = 8000
+    print("Signal processing")
+    for label in labels:
+        files = list(train_audio_path.glob(label + '/*.wav'))
+        print("processing " + label)
+        for file in files:
+            originalSignal, sr = librosa.load(str(file), sr=8000, mono=True)
+            processedSignal = bandpassIIRFilter(originalSignal, sr)
+            processedSignal, index = librosa.effects.trim(processedSignal)
+            duration = librosa.get_duration(processedSignal)
+            sum+=duration
+            processedAudioFiles.append(processedSignal)
+            i+=1
+            all_label.append(label)
+            if sr != 8000:
+                print(sr)
 
-print("tap",train_audio_path)
+    meanDuration=sum/i
+    return processedAudioFiles,sr,meanDuration
 
-labels = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+def resample_dataset(labels,train_audio_path):
+    i = 1
+    durations = []
+    sum = 0
+    processedAudioFiles = []
+    all_label = []
+    sr = 8000
+    print("Signal processing")
+    for label in labels:
+        files = list(train_audio_path.glob(label + '/*.wav'))
+        print("processing " + label)
+        for file in files:
+            originalSignal, sr = librosa.load(str(file), sr=sr, mono=True)
+            processedAudioFiles.append(originalSignal)
+            i+=1
+            all_label.append(label)
+            if sr != 8000:
+                print(sr)
 
-i=1
-durations=[]
-sum=0
-processedAudioFiles=[]
-all_label = []
-sr=8000
-print("Signal processing")
-for label in labels:
-    files = list(train_audio_path.glob(label + '/*.wav'))
-    print("processing " + label)
-    for file in files:
-        originalSignal, sr = librosa.load(str(file), sr=8000, mono=True)
-        processedSignal = bandpassIIRFilter(originalSignal, sr)
-        processedSignal, index = librosa.effects.trim(originalSignal)
-        duration = librosa.get_duration(processedSignal)
-        sum+=duration
-        processedAudioFiles.append(processedSignal)
-        i+=1
-        all_label.append(label)
-        if sr != 8000:
-            print(sr)
-
-meanDuration=sum/i
-
-sumdur=0
-i=0
-
-all_features = []
-desiredNoOfFeatures=13
-# Features Extraction an time normalisation
-print("Extracting Features from "+str(i)+" files, mean duration="+str(meanDuration))
-for audio in processedAudioFiles:
-
-    #sd.play(audio, sr)
-    #status = sd.wait()
-
-    duration = librosa.get_duration(audio)
-    sumdur+=duration
-
-    ratio = duration / meanDuration
-    if ratio < 0.1:
-        ratio = 0.05
-
-    # Stretrch audio to normalise spoken word duration
-    audio = librosa.effects.time_stretch(audio, ratio)
-
-    #sd.play(audio, sr)
-    #status = sd.wait()
-
-    frequency, fourierMagnitude, sampleCount, peaks = fourierTransform(sr, audio, label, no_of_peaks=66)
-
-# audio <- Raw Signal
-# fourierMagnitude <- Fourier representation of the signal (1o tetartimorio)
-
-    all_features.append(fourierMagnitude)
-
-    i+=1
-
-print("Features have been extracted")
-
-
-# Neural Network Classification
-
-all_features = np.array(all_features)
-all_label = np.array(all_label)
-print('Size of X:\n', all_features.shape)     # (77, 8000)
-print('Size of Y:\n', all_label.shape)    # (77,)
-
-x_ = all_features
-y_ = all_label
-
-le = LabelEncoder()
-y_ = le.fit_transform(all_label)
-classes = list(le.classes_)
-
-np.save(current_directory + '/classes_dense.npy', le.classes_)
-y_ = np_utils.to_categorical(y_, num_classes=len(labels))     # From int to one-hot
-print('to_categorical:\n', y_)
-
+    meanDuration=sum/i
+    return processedAudioFiles,all_label,sr,meanDuration
 
 # Neural here
 #x_tr, x_val, y_tr, y_val = train_test_split(np.array(x_), np.array(y_), stratify=y_, test_size=0.2, random_state=777, shuffle=True)
